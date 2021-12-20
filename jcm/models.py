6659564e879b78d6bb4a0117825ff3d7c0e5200b
adighoummed <ghoummed.adi@outlook.com>
2021-12-21 00:54:44 +0200

    Models

--- jcm/models.py
+++ jcm/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    skill = models.ForeignKey('Skill', on_delete=models.SET_NULL, null=True)
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=50)
+    skill = models.ForeignKey('Skill', on_delete=models.SET_NULL, null=True)
