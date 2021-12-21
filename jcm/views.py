9e054ba904112f3a2f9434b7628fc26739d199ea
adighoummed <ghoummed.adi@outlook.com>
2021-12-21 05:32:18 +0200

    First template URL

--- jcm/views.py
+++ jcm/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+from jcm.models import Skill
+
+
+class SkillView(View):
+    def get(self, request):
+        skills_set = Skill.objects.all()
+        context = {
+            "skills": skills_set,
+        }
+
+        return render(request, 'jcm/templates/skills.html', context)
